24537611765d69bf39a4e64f49ccbe34b46028b3
Kevin Chazotte <chazottek@gmail.com>
2025-05-19 22:26:20 -0400

    Building grpc and its dependencies. Modifying install dir structure to facilitate find_package calls on absl and other dependencies with ease. Some modifications to existing projects to make them compatible/extensible.

--- cmake/buildConfig.cmake
+++ cmake/buildConfig.cmake
@@ -1,15 +1,27 @@
 include("${CMAKE_CURRENT_LIST_DIR}/projects.cmake")
 
-function(make_static_imported_library proj libfile_name debug_string)
-  add_library(SuperBuild::${libfile_name} STATIC IMPORTED GLOBAL)
+function(top_level_static_imported_library proj file_name debug_string)
+  add_library(SuperBuild::${proj} STATIC IMPORTED GLOBAL)
   
   # set INTERFACE_INCLUDE_DIRECTORIES instead of INCLUDE_DIRECTORIES on the library
   # see: https://stackoverflow.com/questions/25907478/possible-to-add-an-imported-library-to-target-link-libraries-that-takes-care-of
   # explicitly set imported locations per config that may be generated for the library
+  set_target_properties(SuperBuild::${proj} PROPERTIES
+    IMPORTED_LOCATION_DEBUG "${PROJECT_BUILD_DIR}/x64/Debug/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${file_name}${debug_string}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    IMPORTED_LOCATION_RELWITHDEBINFO "${PROJECT_BUILD_DIR}/x64/RelWithDebInfo/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${file_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    IMPORTED_LOCATION_RELEASE "${PROJECT_BUILD_DIR}/x64/Release/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${file_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
+  set_target_properties(SuperBuild::${proj} PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BUILD_DIR}/x64/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:RelWithDebInfo>/include/"
+  )
+endfunction()
+
+function(make_static_imported_library proj libfile_name debug_string)
+  add_library(SuperBuild::${libfile_name} STATIC IMPORTED GLOBAL)
   set_target_properties(SuperBuild::${libfile_name} PROPERTIES
-    IMPORTED_LOCATION_DEBUG "${PROJECT_BUILD_DIR}/x64/Debug/${proj}/lib/${libfile_name}${debug_string}${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    IMPORTED_LOCATION_RELWITHDEBINFO "${PROJECT_BUILD_DIR}/x64/RelWithDebInfo/${proj}/lib/${libfile_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    IMPORTED_LOCATION_RELEASE "${PROJECT_BUILD_DIR}/x64/Release/${proj}/lib/${libfile_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    IMPORTED_LOCATION_DEBUG "${PROJECT_BUILD_DIR}/x64/Debug/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${libfile_name}${debug_string}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    IMPORTED_LOCATION_RELWITHDEBINFO "${PROJECT_BUILD_DIR}/x64/RelWithDebInfo/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${libfile_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    IMPORTED_LOCATION_RELEASE "${PROJECT_BUILD_DIR}/x64/Release/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${libfile_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
   )
 endfunction()
 
@@ -19,7 +31,7 @@ if(abseil-cpp IN_LIST projects)
   add_library(abseil-cpp INTERFACE)
   add_library(SuperBuild::abseil-cpp ALIAS abseil-cpp)
   file(GLOB ABSEIL_LIBS
-    "${PROJECT_BUILD_DIR}/x64/${CMAKE_BUILD_TYPE}/abseil-cpp/lib/${CMAKE_STATIC_LIBRARY_PREFIX}absl_*.lib"
+    "${PROJECT_BUILD_DIR}/x64/${CMAKE_BUILD_TYPE}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}absl_*${CMAKE_STATIC_LIBRARY_SUFFIX}"
   )
   foreach(absl_lib ${ABSEIL_LIBS})
     get_filename_component(libname ${absl_lib} NAME_WE)
@@ -27,8 +39,26 @@ if(abseil-cpp IN_LIST projects)
     target_link_libraries(abseil-cpp INTERFACE SuperBuild::${libname})
   endforeach()
   set_target_properties(abseil-cpp PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BUILD_DIR}/x64/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:RelWithDebInfo>/abseil-cpp/include/"
+    INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BUILD_DIR}/x64/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:RelWithDebInfo>/include/"
   )
+endif()
+
+if(zlib IN_LIST projects)
+  top_level_static_imported_library(zlib zlib "")
+endif()
+
+if(boringssl IN_LIST projects)
+  top_level_static_imported_library(boringssl boringssl "")
+endif()
+
+if(cares IN_LIST projects)
+  top_level_static_imported_library(cares cares "")
+endif()
+
+if(re2 IN_LIST projects)
+  top_level_static_imported_library(re2 re2 "")
+  add_dependencies(SuperBuild::re2 SuperBuild::abseil-cpp)
+  target_link_libraries(SuperBuild::re2 INTERFACE SuperBuild::abseil-cpp)
 endif()
 
 if(protobuf IN_LIST projects)
@@ -41,11 +71,17 @@ if(protobuf IN_LIST projects)
   make_static_imported_library(protobuf libutf8_validity "")
   add_library(protobuf INTERFACE)
   add_library(SuperBuild::protobuf ALIAS protobuf)
-  add_dependencies(protobuf SuperBuild::abseil-cpp)
+  add_dependencies(protobuf SuperBuild::abseil-cpp SuperBuild::zlib)
   set_target_properties(protobuf PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BUILD_DIR}/x64/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:RelWithDebInfo>/protobuf/include/"
+    INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BUILD_DIR}/x64/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:RelWithDebInfo>/include/"
   )
-  target_link_libraries(protobuf INTERFACE SuperBuild::abseil-cpp
+  target_link_libraries(protobuf INTERFACE SuperBuild::abseil-cpp SuperBuild::zlib
     SuperBuild::libprotobuf SuperBuild::libprotobuf-lite SuperBuild::libprotoc SuperBuild::libupb SuperBuild::libutf8_range SuperBuild::libutf8_validity
   )
+endif()
+
+if(grpc IN_LIST projects)
+  top_level_static_imported_library(grpc grpc "")
+  add_dependencies(SuperBuild::grpc SuperBuild::zlib SuperBuild::protobuf SuperBuild::boringssl SuperBuild::cares SuperBuild::re2)
+  target_link_libraries(SuperBuild::grpc INTERFACE SuperBuild::zlib SuperBuild::protobuf SuperBuild::boringssl SuperBuild::cares SuperBuild::re2)
 endif()
--- cmake/projects.cmake
+++ cmake/projects.cmake
@@ -1,6 +1,6 @@
 unset(projects)
 
-list(APPEND projects abseil-cpp protobuf)
+list(APPEND projects abseil-cpp zlib boringssl cares re2 protobuf grpc)
 
 # abseil-cpp via https://github.com/abseil/abseil-cpp/releases
 set(abseil-cpp_version 20250127.0)
@@ -9,9 +9,44 @@ set(abseil-cpp_url "https://github.com/abseil/abseil-cpp/archive/refs/tags/${abs
 set(abseil-cpp_name "${abseil-cpp_tag}.tar.gz")
 set(abseil-cpp_md5 "dbc174bbc144525c45101a36d4027e7d")
 
+# zlib via https://github.com/madler/zlib/releases
+set(zlib_version 1.3.1)
+set(zlib_tag v1.3.1)
+set(zlib_url "https://github.com/madler/zlib/archive/refs/tags/${zlib_tag}.tar.gz")
+set(zlib_name "${zlib_tag}.tar.gz")
+set(zlib_md5 "ddb17dbbf2178807384e57ba0d81e6a1")
+
+# boringssl via https://github.com/google/boringssl/releases
+set(boringssl_version 0.20250311.0)
+set(boringssl_tag 0.20250311.0)
+set(boringssl_url "https://github.com/google/boringssl/archive/refs/tags/${boringssl_tag}.tar.gz")
+set(boringssl_name "${boringssl_tag}.tar.gz")
+set(boringssl_md5 "2716ecfdee919512ca372b0ada512f7a")
+
+# cares via https://github.com/c-ares/c-ares/releases
+set(cares_version 1.34.5)
+set(cares_tag v1.34.5)
+set(cares_url "https://github.com/c-ares/c-ares/archive/refs/tags/${cares_tag}.tar.gz")
+set(cares_name "${cares_tag}.tar.gz")
+set(cares_md5 "ba8e0067b17fad5cba87d3b047e906d2")
+
+# re2 via https://github.com/google/re2/releases
+set(re2_version 2024-07-02)
+set(re2_tag 2024-07-02)
+set(re2_url "https://github.com/google/re2/archive/refs/tags/${re2_tag}.tar.gz")
+set(re2_name "${re2_tag}.tar.gz")
+set(re2_md5 "73c94c78678bd2d641beaf8d5a3fd352")
+
 # protobuf via https://github.com/protocolbuffers/protobuf/releases
 set(protobuf_version 30.0)
 set(protobuf_tag v30.0)
 set(protobuf_url "https://github.com/protocolbuffers/protobuf/archive/refs/tags/${protobuf_tag}.tar.gz")
 set(protobuf_name "${protobuf_tag}.tar.gz")
 set(protobuf_md5 "63f3d276a8114f28ee1649443935e4af")
+
+# grpc via https://github.com/grpc/grpc/releases
+set(grpc_version 1.71.0)
+set(grpc_tag v1.71.0)
+set(grpc_url "https://github.com/grpc/grpc/archive/refs/tags/${grpc_tag}.tar.gz")
+set(grpc_name "${grpc_tag}.tar.gz")
+set(grpc_md5 "89ad442e1b174bc5d55c554aec583fa0")
