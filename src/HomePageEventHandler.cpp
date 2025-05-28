1e131b502ac84012bdb3a3e54253dbf0a5ead1d5
Kevin Chazotte <chazottek@gmail.com>
2025-05-27 23:15:54 -0400

    Fixing bug where home page buttons are only partially interactive

--- src/HomePageEventHandler.cpp
+++ src/HomePageEventHandler.cpp
@@ -13,14 +13,14 @@ void HomePageEventHandler::handleEvent(const sf::Event& event, WhiteboardStateMa
             sf::Vector2f mousePosition = sf::Vector2f(mouseButtonPressed->position.x, mouseButtonPressed->position.y);
             // crude test to see if mouse position is on the button, update to be more dynamic
             sf::FloatRect connectButtonArea = sf::FloatRect(sf::Vector2f(m_RenderWindow.getSize().x / 2 - kButtonWidth / 2, m_RenderWindow.getSize().y / 2),
-                sf::Vector2f(kButtonHeight, kButtonHeight));
+                sf::Vector2f(kButtonWidth, kButtonHeight));
             if (connectButtonArea.contains(mousePosition)) {
                 m_ServerManager->connect(); // once connection completes, update signal to emit new app state
                 currentState = WhiteboardStateMachine::AppState::kWhiteboard; // for now, manually update app state
                 return;
             }
             sf::FloatRect exitButtonArea = sf::FloatRect(sf::Vector2f(m_RenderWindow.getSize().x / 2 - kButtonWidth / 2, m_RenderWindow.getSize().y / 1.6),
-                sf::Vector2f(kButtonHeight, kButtonHeight));
+                sf::Vector2f(kButtonWidth, kButtonHeight));
             if (exitButtonArea.contains(mousePosition)) {
                 m_RenderWindow.close();
                 return;
