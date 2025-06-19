95ee7190771fca55f4d58f77628f83f73430e569
Kevin Chazotte <chazottek@gmail.com>
2025-06-18 23:07:54 -0400

    Adding command-line argument for server address to allow for input path. Enabling non-local networking

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -4,8 +4,11 @@
 #include "ServerConnectionManager.h"
 #include "WhiteboardClientApp.h"
 
-int main() {
+int main(int argc, char** argv) {
     std::string server_address("localhost:50052");
+    if (argc > 1) {
+        server_address = argv[1];
+    }
     std::shared_ptr<ServerConnectionManager> serverConnectionManager = std::make_shared<ServerConnectionManager>(server_address);
     WhiteboardClientApp clientApp = WhiteboardClientApp(serverConnectionManager);
     clientApp.Run();
